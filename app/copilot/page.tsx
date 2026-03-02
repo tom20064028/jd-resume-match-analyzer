1d0ae6df5ece4c526b8aa50ea952ea647e3daba0
Ka Ho Chan <tommykaho@hotmail.com>
2026-03-02 11:02:50 +0100

    Day 14: Add form submission logic to CopilotPage for JD and resume analysis

--- app/copilot/page.tsx
+++ app/copilot/page.tsx
@@ -1,4 +1,32 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
 export default function CopilotPage() {
+
+    useEffect(() => {
+        const form = document.querySelector("form");
+        form?.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            // setLoading(true);
+            const formData = new FormData(form);
+            const jd = formData.get("jd");
+            const resume = formData.get("resume");
+            const res1 = await fetch("/api/analyze", {
+                method: "POST",
+                body: JSON.stringify({ jd, resume }),
+            })
+            const analyzeData = await res1.json();
+            const res2 = await fetch("/api/interview", {
+                method: "POST",
+                body: JSON.stringify({ jd }),
+            })
+            const interviewData = await res2.json();
+            console.log({ analyzeData, interviewData });
+
+        });
+    }, []);
+
     return (
 
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -12,12 +40,14 @@ export default function CopilotPage() {
                         <div className="flex flex-col gap-2">
                             <label htmlFor="jd">JD</label>
                             <textarea
+                                name="jd" id="jd" 
                                 placeholder="Paste job description" className="w-full border-2 border-gray-300 rounded-md p-2" rows={10} 
                             />
                         </div>
                         <div className="flex flex-col gap-2">
                             <label htmlFor="resume">Resume</label>
                             <textarea
+                                name="resume" id="resume"
                                 placeholder="Paste your resume" className="w-full border-2 border-gray-300 rounded-md p-2" rows={10} 
                             />
                         </div>
