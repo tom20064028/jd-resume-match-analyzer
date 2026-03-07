9363fb96295eb9633a561ebec4828f3697169951
Ka Ho Chan <tommykaho@hotmail.com>
2026-03-07 11:02:11 +0100

    Day 21: Add InterviewPage component: Implement job description input form and initial interview stage handling

---
+++ app/interview-simulator/page.tsx
@@ -0,0 +1,103 @@
+'use client';
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
+export default function InterviewPage() {
+    const exampleJD = `We are looking for a frontend developer with React, TypeScript, and API experience...`;
+
+    const [jd, setJd] = useState("")
+    const [result, setResult] = useState<any>(null)
+    const [loading, setLoading] = useState(false);
+    const [stage, setStage] = useState(0)
+
+    useEffect(() => {
+        const jdForm = document.querySelector("#jd-form");
+        jdForm?.addEventListener("submit", (e) => {
+          e.preventDefault();
+          setStage(1)
+        //   setLoading(true);
+        //   const formData = new FormData(form);
+        //   const jd = formData.get("jd");
+        //   fetch("/api/interview", {
+        //     method: "POST",
+        //     body: JSON.stringify({ jd }),
+        //   }).then(res => res.json()).then(data => {
+        //     let parsed;
+    
+        //     try {
+        //       parsed = JSON.parse(data.raw);
+        //     } catch {
+        //       parsed = {
+        //         questions: []
+        //       };
+        //     }
+        //     console.log(parsed)
+        //     setResult(parsed);
+        //     setLoading(false);
+        //   }).catch(err => {
+        //     console.error(err);
+        //     setLoading(false);
+        //   });
+        });
+    }, []);
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+            
+                <div className="flex flex-col gap-4 w-full">
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-4xl font-bold">AI Interview Simulator</h1>
+                        <p className="text-lg text-gray-500">
+                            Paste a JD here and start your interview.
+                        </p>
+                    </div>
+                    { stage === 0 && (
+                        <form id="jd-form" className="flex flex-col gap-4">
+                            <div className="flex flex-col gap-2">
+                                <label htmlFor="jd">JD</label>
+                                <textarea placeholder="Paste job description" name="jd" id="jd" className="w-full border-2 border-gray-300 rounded-md p-2" rows={10} required value={jd} onChange={(e) => setJd(e.target.value)} />
+                            </div>
+                            <div className="grid grid-cols-2 gap-2 w-full">
+                                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">Start</button>
+                                <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer" type="button" onClick={() => setJd(exampleJD)}>
+                                    Use example
+                                </button>
+                            </div>
+                    
+                            {/* {result && (
+                            <div className="flex flex-col gap-2 mt-4">
+                                <h2 className="text-2xl font-bold">Result</h2>         
+                                <div className="mt-4">
+                                {result.questions?.length > 0 && (
+                                    <>
+                                    <h3 className="text-lg font-semibold mt-4">Questions</h3>
+                                    <ul>
+                                        {result.questions?.map((s: string, i: number) => (
+                                        <li className="list-disc list-inside" key={i}>{s}</li>
+                                        ))}
+                                    </ul>
+                                    </>
+                                )}
+                                </div>
+                            </div>
+                            )} */}
+                        </form>
+                    )}
+                    { stage === 1 && (
+                        <form className="flex flex-col gap-4">
+                            <h2 className="text-xl mt-8">How are you?</h2>
+                            <div className="flex flex-col gap-2 mt-8">
+                                <label htmlFor="answer">Answer</label>
+                                <textarea placeholder="Input your answer" name="answer" id="answer" className="w-full border-2 border-gray-300 rounded-md p-2" rows={10} required />
+                            </div>
+                            <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">Answer</button>
+                        </form>
+                    )}
+                </div>
+            </main>
+        </div>
+    );
+  }
